990f441ffdc77b10b06f8921a33f370099ee6aac
thammer <tobias@hammerhome.no>
2025-12-09 01:20:54 +0100

    Fix: Remove unused useCallback import from chat.js

--- eksamen/client/src/pages/chat.js
+++ eksamen/client/src/pages/chat.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import styles from "../chat.module.css";
 import { API_ENDPOINTS } from "../config";
